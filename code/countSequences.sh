395d384588a3257c97e0d8797e6eacbd37dd16a7
Chelsea Noack <cjnoack@usfca.edu>
2017-09-09 13:20:27 -0700

    Modify code similar to BDS text

--- code/countSequences.sh
+++ code/countSequences.sh
@@ -7,10 +7,9 @@
 
 # Count the number of sequences in our fasta file
 
-for file in $@
+for file in $@ 
 do 
-
-	echo "This file has $(grep ">"  animal_mRNA.fasta | wc -l) sequences"
-	echo "This file has $(grep ">" crab_mitochondrial_rRNA.fasta | wc -l) sequences"
-	echo "This file has $(grep ">" plant_rRNA.fasta wc -l) sequences"
+	echo "This file has $(grep -c "^>" animal_mRNA.fasta | wc -l) sequences"
+	echo "This file has $(grep -c "^>" crab_mitochondrial_rRNA.fasta | wc -l) sequences"
+	echo "This file has $(grep -c "^>" plant_rRNA.fasta wc -l) sequences"
 done
